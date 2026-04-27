ed24d9435bb43ec4a5fb7f9083a52861709a52cb
Vikorikus <Vikorikus@gmail.com>
2026-04-27 19:47:23 +0300

    ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hhappclone/",
   plugins: [react()],
   test: {
     // Позволяет использовать методы типа describe, it, expect без импорта в каждом файле
